a42701e824cf05f3a73e5b4e5ec1b8cd353ced93
sirancao <cao.sir@northeastern.edu>
2023-03-31 11:19:15 -0700

    a8 REST - fixed post summary list

--- src/tuiter/tuits/TuitItem.js
+++ src/tuiter/tuits/TuitItem.js
@@ -9,18 +9,11 @@ import {deleteTuitThunk} from "../../services/tuits-thunks";
 const TuitItem = (
     {
         post = {
-            "_id": 123,
-            "topic": "Space",
-            "userName": "SpaceX",
-            "title": "Tesla Cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed",
-            "time": "2h",
-            "image": "tesla",
-            "liked": true,
-            "replies": 123,
-            "retuits": 432,
-            "likes": 2345,
-            "handle": "@spacex",
-            "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
+            "_id": "123", "topic": "Space", "username": "SpaceX",
+            "handle": "@spacex", "time": "2h", "image": "spacex.jpg",
+            "title": "SpaceX's Mission",
+            "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+            "liked": true, "likes": 2345, "replies": 123, "retuits": 432, "dislikes": 678, "disliked": false
         }
     }
 ) => {
@@ -38,7 +31,7 @@ const TuitItem = (
                 <div className="col ms-3">
                     <div className="d-flex justify-content-between">
                         <div className="flex-column">
-                            <b>{post.userName}</b><FontAwesomeIcon icon={faCheckCircle} className="ms-1 me-2 text-primary"/>
+                            <b>{post.username}</b><FontAwesomeIcon icon={faCheckCircle} className="ms-1 me-2 text-primary"/>
                             <span className="text-dark">@ {post.topic} · {post.time}</span>
                         </div>
                         <i className="bi bi-x-lg float-end clickable"
